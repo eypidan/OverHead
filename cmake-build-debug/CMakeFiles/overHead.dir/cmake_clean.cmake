dfc5dd876e57f615ea753620fc09497ea5fe6601
Pang Siyuan <eypidanh@163.com>
2019-09-25 19:49:20 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/overHead.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/overHead.dir/main.cpp.obj"
+  "overHead.pdb"
+  "overHead.exe"
+  "overHead.exe.manifest"
+  "liboverHead.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/overHead.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
