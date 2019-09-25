e1ea943e82fef3bc879416958df592b9c110f6b6
Pang Siyuan <eypidanh@163.com>
2019-09-25 20:53:31 +0800

    init CMakeLIst

---
+++ README.md
@@ -0,0 +1,29 @@
+## OverHead
+
+- An opengl program to simulate the view of a plane taking off.
+
+### Install & Run
+
+- Make sure your PC's opengl version >= 4.5.
+  -  WINDOWS:[OpenGL Extension Viewer](http://download.cnet.com/OpenGL-Extensions-Viewer/3000-18487_4-34442.html) .
+  -  Linux: call  **glxinfo**.
+
+- Download GLFW and install it to your environment.
+
+  - For me in windows: (I use MinGW and cmake to compile).
+
+  - Ensure your mingw/bin directory is in your global environment .
+
+  - ```shell
+    cd /path/to/GLFW
+    cmake -G "MinGW Makefiles" -S . -B . -DCMAKE_INSTALL_PREFIX="C:\mingw64\x86_64-w64-mingw32" -DCMAKE_CXX_COMPILER:FILEPATH=C:/mingw64/bin/g++.exe -DCMAKE_C_COMPILER:FILEPATH=C:/mingw64/bin/gcc.exe
+    mingw32-make all
+    mingw32-make install
+    ```
+
+  - Use `mingw32-make clean` to eliminate your build about glfw.
+
+- **glad** is a web service, please go this [web service](http://glad.dav1d.de/) to download opengl in 4.5 version.
+
+  
+
--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
+//opengl version 4.5
+//
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "OverHead", NULL, NULL);
+    if(window == NULL){
+        std::cout << "Failed to create GLFW windows" <<std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+    glViewport(0, 0, 800, 600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();  //check input event like keyboard or mouse
+    }
+    glfwTerminate();
+
     return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
 }
