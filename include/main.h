213a993eb89ee5c0b1b587623da93e9898c5b5c0
Pang Siyuan <eypidanh@163.com>
2019-10-04 23:54:14 +0800

    try shader class to beatify the code structure

--- include/main.h
+++ include/main.h
@@ -9,7 +9,23 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <fstream>
+#include <sstream>
 #include <math.h>
+
+class Shader {
+public:
+    // the program ID
+    unsigned int ID;
+
+    // constructor reads and builds the shader
+    Shader(const GLchar* vertexPath, const GLchar* fragmentPath);
+    // use/activate the shader
+    void use();
+    // utility uniform functions
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+};
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
--- src/function.cpp
+++ src/function.cpp
@@ -33,4 +33,44 @@ std::string readFile(const char *filePath){
     return content;
 }
 
+Shader (const char* vertexPath, const char* fragmentPath){
+    //1. retrieve the vertex/fragment source code from filePath
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try{
+        //open files;
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        std::stringstream vShaderStream,fShaderStream;
 
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+
+        vShaderFile.close();
+        fShaderFile.close();
+        //convert stream to string
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+    }
+    catch(std::ifstream::failure e){
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+    }
+    const char* vShaderCode = vertexCode.c_str();
+    const char *fShaderCode = fragmentCode.c_str();
+
+    //2.compile shaders
+    unsigned int vertex,fragment;
+    int success;
+    char infoLog[512];
+
+    //vertex Shader
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex,1,&vShaderCode,NULL);
+    glCompileShader(vertex);
+    //if error, print
+    glGetShaderiv(vertex,GL_COMPILE_STATUS,&success);
+}
