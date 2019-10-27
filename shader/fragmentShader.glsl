7987252e2b28fc0861de3033afbdfa931291f889
Pang Siyuan <eypidanh@163.com>
2019-10-27 14:05:29 +0800

    add house

--- shader/fragmentShader.glsl
+++ shader/fragmentShader.glsl
@@ -5,8 +5,8 @@ in vec2 TexCoord;
 in vec3 Normal;
 in vec3 FragPos;
 // texture samplers
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+uniform sampler2D textureInstance;
+
 uniform vec3 lightPos;
 uniform vec3 lightColor;
 uniform vec3 viewPos;
@@ -31,5 +31,5 @@ void main()
     vec3 specular = specularStrength * spec * lightColor;
 
     // linearly interpolate between both textures (80% container, 20% awesomeface)
-    FragColor =vec4(diffuse+ambient+specular,1.0)*mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+    FragColor =vec4(diffuse+ambient+specular,1.0)*texture(textureInstance, TexCoord);
 }
