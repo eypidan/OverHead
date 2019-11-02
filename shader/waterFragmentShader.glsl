f4cd96daeb7d690420d2dcdbe1a48f742ebea4ad
Pang Siyuan <eypidanh@163.com>
2019-11-02 11:00:25 +0800

    draw infinite plane

--- shader/waterFragmentShader.glsl
+++ shader/waterFragmentShader.glsl
@@ -3,5 +3,5 @@ out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(0.2,0.3,0.4,1.0); // set alle 4 vector values to 1.0
+    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f); // set alle 4 vector values to 1.0
 }
